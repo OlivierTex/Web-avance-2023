ac559cd8cc83d562419b3e2e773aba7cf81f1925
Lotharim <greg.demirdjian@aim.com>
2023-09-17 19:33:38 +0200

    fix: correction scripts JS

--- Labs/Lab2/handles.js
+++ Labs/Lab2/handles.js
@@ -3,14 +3,19 @@
 module.exports = {
     serverHandle: function (req, res) {
 
+
         const url = require('url')
         const qs = require('querystring')
 
           const route = url.parse(req.url)
           const path = route.pathname 
           const params = qs.parse(route.query)
+          console.log(path)
+          const queryParams = qs.parse(url.parse(req.url).query)
+          console.log(queryParams)
         
-          res.writeHead(200, {'Content-Type': 'text/plain'})
+          res.writeHead(200, {'Content-Type': 'text/html'})
+          
         
           if (path === '/hello' && 'name' in params) {
             res.write('Hello ' + params['name'])
--- Labs/Lab2/index.js
+++ Labs/Lab2/index.js
@@ -1,5 +1,17 @@
 // ./index.js
 
+// Define a string constant concatenating strings
+const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>ECE AST</title>' +
+'    </head>' + 
+'    <body>' +
+'       <p>Hello World!</p>' +
+'    </body>' +
+'</html>'
+
 const http = require('http')
 const handles = require('./handles')
 
