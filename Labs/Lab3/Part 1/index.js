f04ba3b4b62cf3a2c27298b136414aa71d75e630
Lotharim <greg.demirdjian@aim.com>
2023-09-26 12:24:05 +0200

    fix: Lab2 using Express

---
+++ Labs/Lab3/Part 1/index.js
@@ -0,0 +1,39 @@
+// ./index.js
+
+// Define a string constant concatenating strings
+const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>ECE AST</title>' +
+'    </head>' + 
+'    <body>' +
+'       <p>Hello World!</p>' +
+'    </body>' +
+'</html>'
+
+const handles = require('./handles')
+
+const express = require('express')
+const app = express()
+
+app.set('port', 8080)
+
+
+app.get('/hello', function (req, res) {
+    res.send("Vous pouvez visiter : <br/> -> /hello/SaisirUnNom pour dire bonjour a la personne <br/> -> /hello/SaisirNomAuteur pour avoir plus d'informations sur l'auteur")
+ })
+
+
+app.get('/hello/:name', function (req, res) {
+    if (req.params.name === 'Greg')
+        res.send("Bonjour, je m'appelle Greg et je suis l'auteur de cette petite application. <br/>Celle-ci est creee dans le cadre du cours de Technologies Web a l'ECE Paris.")
+    else
+        res.send("Hello " + req.params.name)
+})
+
+
+app.listen(
+  app.get('port'), 
+  () => console.log(`server listening on ${app.get('port')}`)
+)
