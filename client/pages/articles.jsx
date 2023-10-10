a8a8abee7e11368a3639927ca042a806ea1ecf79
Lotharim <greg.demirdjian@aim.com>
2023-10-10 14:34:25 +0200

    fix: tests tailwindcss

--- client/pages/articles.jsx
+++ client/pages/articles.jsx
@@ -4,12 +4,13 @@ import articles from '../data/data';
 
   export default function Articles() {
     return (
+      <div className='bg-gray-500'> 
       <Layout>
-        <h1>Articles</h1>
+        <h1>Articles<br /><br /></h1>
         <ul>
         {articles.map((article) => (
         <li key={article.id}>
-          <Link href={`/articles/${article.id}`}>
+          <Link className = 'italic font-bold text-2xl text-blue-900 underline'href={`/articles/${article.id}`}>
             {article.title}
           </Link>
         </li>
@@ -17,5 +18,6 @@ import articles from '../data/data';
 
         </ul>
       </Layout>
+      </div>
     );
   }
