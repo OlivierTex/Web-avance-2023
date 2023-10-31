62f685655666eedb28ad70f4059fcfe4b5ba91c4
Lotharim <greg.demirdjian@aim.com>
2023-10-31 02:30:20 +0100

    feat: ajout de plus de styles aux pages avec tailwind

--- client/pages/about.js
+++ client/pages/about.js
@@ -4,9 +4,9 @@ import Layout from '../components/layout'
 export default function About() {
     return (
       <Layout>
-      <h1>About</h1>
+      <h1 className="text-3xl text-center mt-8">About</h1>
       {
-        <div>Cette application est écrite dans le cadre du cours de Tech Web de l'ECE Paris par Greg, Olivier et Marc</div>
+        <div className="text-lg text-center text-gray-700 mt-4">Cette application est écrite dans le cadre du cours de Tech Web de l'ECE Paris par Greg, Olivier et Marc</div>
       }
     </Layout>
     )
--- client/pages/contacts.js
+++ client/pages/contacts.js
@@ -3,9 +3,9 @@ import Layout from '../components/layout';
 export default function Contacts() {
   return (
     <Layout>
-      <h1>Contacts</h1>
+      <h1 className="text-3xl text-center mt-8">Contacts</h1>
 
-      <form>
+      <form className="mt-8">
         <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
           Nom :
         </label>
@@ -14,25 +14,31 @@ export default function Contacts() {
           id="name"
           name="name"
           placeholder="Saisissez votre nom"
-          className="form-input mt-1 p-2 w-full"
+          className="form-input mt-1 p-2 w-full border-2 border-gray-300 rounded-md"
         />
 
         <button
           type="submit"
-          className="bg-blue-500 text-white p-2 mt-4 hover:bg-blue-700 transition-all duration-300"
+          className="bg-blue-500 text-white p-2 mt-4 hover:bg-blue-700 transition-all duration-300 rounded-md"
         >
           Envoyer
         </button>
       </form>
 
-      <div>
-        Contacts :
+      <div className="text-center mt-8 text-gray-700">
+        <p className="text-lg">Contacts :</p>
         <br />
         - Greg Demirdjian
         <br />
         - Marc Hamchouchong
         <br />
         - Olivier Texier
+      </div>
+
+      <div className="text-center mt-8">
+        <p className="text-lg">Adresse : XXX Rue de XXXX, 75000 Paris</p>
+        <p className="text-lg">Téléphone : +33 X XX XX XX XX</p>
+        <p className="text-lg">Email : xxx@xxx.com</p>
       </div>
     </Layout>
   );
