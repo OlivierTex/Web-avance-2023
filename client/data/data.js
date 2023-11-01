45f6867f7981b71e6fa9e7acaabd61b95b9f2eec
Lotharim <greg.demirdjian@aim.com>
2023-11-01 18:57:03 +0100

    feat: Catalogue photos pour les articles, avec boutons pour choisir le type d'affichage

--- client/data/data.js
+++ client/data/data.js
@@ -1,7 +1,10 @@
 const articles = [
-    { id: 1, title: 'Article 1', content: 'This is Article 1' },
-    { id: 2, title: 'Article 2', content: 'This is Article 2' },
-    { id: 3, title: 'Article 3', content: 'This is Article 3' },
-  ];
-  
-  export default articles;
+  { id: 1, title: 'Article 1', content: 'This is Article 1', image: 'https://images.unsplash.com/photo-1594322436404-5a0526db4d13?auto=format&fit=crop&q=80&w=2029&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 2, title: 'Article 2', content: 'This is Article 2', image: 'https://images.unsplash.com/photo-1594322436404-5a0526db4d13?auto=format&fit=crop&q=80&w=2029&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 3, title: 'Article 3', content: 'This is Article 3', image: 'https://images.unsplash.com/photo-1594322436404-5a0526db4d13?auto=format&fit=crop&q=80&w=2029&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 4, title: 'Article 4', content: 'This is Article 4', image: 'https://images.unsplash.com/photo-1594322436404-5a0526db4d13?auto=format&fit=crop&q=80&w=2029&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 5, title: 'Article 5', content: 'This is Article 5', image: 'https://images.unsplash.com/photo-1594322436404-5a0526db4d13?auto=format&fit=crop&q=80&w=2029&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  { id: 6, title: 'Article 6', content: 'This is Article 6', image: 'https://images.unsplash.com/photo-1594322436404-5a0526db4d13?auto=format&fit=crop&q=80&w=2029&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+];
+
+export default articles;
--- client/pages/articles.js
+++ client/pages/articles.js
@@ -1,51 +1,73 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import Layout from '../components/layout';
 import articlesData from '../data/data';
-import React, { useEffect, useState } from 'react';
-
 
 export default function Articles(articles) {
-  const [profile, setProfile] = useState(null);
+  const [articlesPerRow, setArticlesPerRow] = useState(4);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/profile')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setProfile(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching profile:', error);
-      });
-  }, []);
+  const handleArticlesPerRowChange = (count) => {
+    setArticlesPerRow(count);
+  };
 
   return (
     <div>
       <Layout>
-        <h1 className="text-3xl text-center mt-8">Articles</h1>
-        <ul className="mt-8">
-          {articlesData.map((article) => (
-            <li key={article.id} className="text-center">
-              <Link className="text-blue-900 hover:underline" href={`/articles/${article.id}`}>
-                {article.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <h1 className="text-3xl text-center mt-8">Catalogue photos</h1>
+        <div className="flex justify-end mt-4 space-x-4">
+          <button
+            onClick={() => handleArticlesPerRowChange(2)}
+            className={`${
+              articlesPerRow === 2 ? 'bg-blue-900 text-white' : 'bg-gray-200'
+            } px-4 py-2 rounded-md`}
+          >
+            2 par ligne
+          </button>
+          <button
+            onClick={() => handleArticlesPerRowChange(4)}
+            className={`${
+              articlesPerRow === 4 ? 'bg-blue-900 text-white' : 'bg-gray-200'
+            } px-4 py-2 rounded-md`}
+          >
+            4 par ligne
+          </button>
+          <button
+            onClick={() => handleArticlesPerRowChange(6)}
+            className={`${
+              articlesPerRow === 6 ? 'bg-blue-900 text-white' : 'bg-gray-200'
+            } px-4 py-2 rounded-md`}
+          >
+            6 par ligne
+          </button>
+        </div>
+        <div className="w-11/12 mx-auto">
+          <div className="flex flex-wrap justify-center mt-8">
+            {articlesData.map((article) => (
+              <div
+                key={article.id}
+                className={`w-80 p-4 ${
+                  articlesPerRow === 2
+                    ? 'w-1/2'
+                    : articlesPerRow === 4
+                    ? 'w-1/4'
+                    : 'w-1/6'
+                }`}
+              >
+                <Link
+                  href={`/articles/${article.id}`}
+                  className="block h-full border border-gray-300 p-2 hover:bg-gray-100"
+                >
+                  <img
+                    src={article.image}
+                    alt={article.title}
+                    className="w-full object-cover"
+                  />
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
       </Layout>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const articles = articlesData;
-  return {
-    props: {
-      articles,
-    },
-  };
 }
