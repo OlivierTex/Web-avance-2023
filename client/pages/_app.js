a68b78acecdc72fca290d706621705bdc03dc04f
Olivier Texier <olivier@MacBook-Pro-de-Olivier.local>
2023-11-04 10:30:31 +0100

    Fix: mode sombre

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,10 +3,12 @@ import '../styles/globals.css';
 
 function App({ Component, pageProps }) {
   return (
+    <div className={`bg-light dark:bg-dark`}>
     <div className="body">
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </div>
     </div>
   );
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -3,13 +3,11 @@ import Navigation from '../components/navigation';
 function Home() {
   return (
     <div className={`bg-light dark:bg-dark`}>
-
-      <h1 className="h1">Bienvenue sur notre site</h1>
+      <h1 className="btn-primary">Bienvenue sur notre site</h1>
       <p className="paragraphe">Découvrez notre sélection de photos de haute qualité.</p>
       <div className="mt-8">
       <Navigation/>
       </div>
-      
     </div>
 
   );
