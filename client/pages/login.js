ae82afa655a33b7cd2b2c67149291287479ebe0f
Lotharim <greg.demirdjian@aim.com>
2023-11-07 20:59:28 +0100

    feat: intégration et test api supabase

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,22 +1,50 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
+import { supabase } from '../supabase'
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const fetchData = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('users')
+        .select('*');
+
+      if (error) {
+        throw error;
+      }
+
+      if (data) {
+        console.log('Données récupérées depuis Supabase :', data);
+      }
+    } catch (error) {
+      console.error('Une erreur s\'est produite lors de la récupération des données:', error.message);
+    }
+  };
+
   const handleLogin = async (e) => {
     e.preventDefault();
-   
-    console.log('Login credentials', { email, password });
-   
+
+    const { user, session, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      console.error('Une erreur s\'est produite lors de la connexion:', error.message);
+    } else {
+      console.log('Utilisateur connecté:', user);
+      console.log('Session:', session);
+    }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-light dark:bg-dark">
       <div className="max-w-md w-full space-y-8 p-10 bg-white rounded-xl shadow-lg z-10">
         <div className="text-center">
-          <h2 className="h2">
+          <h2 className="h2" onClick={fetchData}>
             Bonjour 
           </h2>
           <p className="mt-2 text-sm text-gray-600">
