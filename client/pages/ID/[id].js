5160ab2a4a672a8b5a242705073e0b5be9f89be0
Olivier Texier <olivier@MacBook-Pro-de-Olivier.local>
2023-11-08 20:33:07 +0100

    feat : ajout fonctionnalite compte admin supression et ajout de compte

--- client/pages/ID/[id].js
+++ client/pages/ID/[id].js
@@ -2,28 +2,64 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { getAPIKey, getAPIBaseURL } from '../../API/API_pexels';
+import supabase from '../../supabase';
 
 const ImageDetail = () => {
   const router = useRouter();
-  const { id } = router.query;
+  const { id } = router.query; 
   const [imageDetails, setImageDetails] = useState(null);
   const [isImageFullscreen, setIsImageFullscreen] = useState(false);
 
+  const checkAndUpdateViews = async (apiImageId) => {
+    try {
+      const { data, error } = await supabase
+        .from('images')
+        .select('*')
+        .eq('api_image_id', apiImageId);
+  
+      if (error) {
+        console.error('Error fetching image:', error);
+        return;
+      }
+  
+      if (data.length > 0) {
+        const imageData = data[0];
+        const { error: updateError } = await supabase
+          .from('images')
+          .update({ views: imageData.views + 1 })
+          .eq('api_image_id', apiImageId);
+  
+        if (updateError) {
+          console.error('Error updating image views:', updateError);
+        }
+      } else {
+        const { error: insertError } = await supabase
+          .from('images')
+          .insert([{ api_image_id: apiImageId, views: 1 }]);
+  
+        if (insertError) {
+          console.error('Error inserting new image:', insertError);
+        }
+      }
+    } catch (error) {
+      console.error('Error in checkAndUpdateViews:', error);
+    }
+  };
+
   useEffect(() => {
-    if (!router.isReady) return;
+    if (!router.isReady || !id) return;
 
     const fetchImageDetails = async () => {
       const apiKey = getAPIKey();
       const baseUrl = getAPIBaseURL();
-      const url = `${baseUrl}photos/${id}`;
+      const url = `${baseUrl}/photos/${id}`;
 
       try {
         const response = await axios.get(url, {
-          headers: {
-            Authorization: apiKey,
-          },
+          headers: { Authorization: apiKey }
         });
         setImageDetails(response.data);
+        checkAndUpdateViews(id);
       } catch (error) {
         console.error('Fetching image details failed:', error);
       }
@@ -35,46 +71,36 @@ const ImageDetail = () => {
   useEffect(() => {
     const handleOutsideClick = (e) => {
       if (isImageFullscreen && e.target.tagName !== 'IMG') {
-        toggleFullscreen();
+        setIsImageFullscreen(false);
       }
     };
 
     if (isImageFullscreen) {
       document.addEventListener('click', handleOutsideClick);
       document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
     }
 
     return () => {
       document.removeEventListener('click', handleOutsideClick);
-      document.body.style.overflow = 'auto';
+      if (!isImageFullscreen) {
+        document.body.style.overflow = 'auto';
+      }
     };
   }, [isImageFullscreen]);
 
-  const toggleFullscreen = () => {
-    setIsImageFullscreen(!isImageFullscreen);
-  };
-
   const handleDownload = async (url, filename) => {
     try {
-      const response = await axios.get(url, {
-        responseType: 'blob', 
-      });
-
+      const response = await axios.get(url, { responseType: 'blob' });
       const blobUrl = window.URL.createObjectURL(new Blob([response.data]));
-
       const link = document.createElement('a');
       link.href = blobUrl;
       link.setAttribute('download', filename);
-
       document.body.appendChild(link);
       link.click();
-
       link.parentNode.removeChild(link);
       window.URL.revokeObjectURL(blobUrl);
     } catch (error) {
-      console.error('Error during image download', error);
+      console.error('Error during image download:', error);
     }
   };
 
@@ -83,57 +109,53 @@ const ImageDetail = () => {
   }
 
   return (
-      <div className="body">
-        <br></br>
+    <div className="body">
+      <div className="relative">
+        <p className="text-center"><strong>Image Name:</strong> {imageDetails.alt}</p>
+        <button 
+          onClick={() => handleDownload(imageDetails.src.original, `${imageDetails.alt}.jpeg`)}
+          className="absolute top-0 right-0 bg-gray-800 text-white px-2 py-1 rounded-md text-xs"
+        >
+          Download Image
+        </button>
+      </div>
+      <div onClick={() => setIsImageFullscreen(!isImageFullscreen)} className="cursor-pointer">
+        <img 
+          src={imageDetails.src.original} 
+          alt={imageDetails.alt} 
+          className="w-auto h-auto object-cover mx-auto max-h-screen mb-4 cursor-pointer border-4 border-custom1" 
+        />
+      </div>
+      <div className="space-y-2 text-center">
+        <p>Photographer: {imageDetails.photographer}</p>
+        <p>Photographer URL: <a href={imageDetails.photographer_url} target="_blank" rel="noreferrer">{imageDetails.photographer_url}</a></p>
+        <p>Image URL: <a href={imageDetails.url} target="_blank" rel="noreferrer">{imageDetails.url}</a></p>
+        <p>Dimensions: {imageDetails.width} x {imageDetails.height}</p>
+      </div>
+      {isImageFullscreen && (
+        <div 
+          className="fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-75 flex justify-center items-center" 
+          onClick={() => setIsImageFullscreen(false)} 
+        >
           <div className="relative">
-          <p className="text-center"><strong>Image Name:</strong> {imageDetails.alt}</p>
-          <button 
-            onClick={() => handleDownload(imageDetails.src.original, `${imageDetails.alt}.jpeg`)}
-            className="absolute top-0 right-0 bg-gray-800 text-white px-2 py-1 rounded-md text-xs"
-          >
-            Download Image
-          </button>
-          </div>
-             <br></br>
-          <div onClick={toggleFullscreen} className="cursor-pointer">
             <img 
               src={imageDetails.src.original} 
               alt={imageDetails.alt} 
-              className="w-auto h-auto object-cover mx-auto max-h-screen mb-4 cursor-pointer border-4 border-custom1" 
+              className="h-screen mx-auto" 
             />
+            <button 
+              onClick={(e) => {
+                e.stopPropagation(); 
+                setIsImageFullscreen(false);
+              }} 
+              className="absolute top-4 right-4 text-white text-3xl focus:outline-none"
+            >
+              ×
+            </button>
           </div>
-          <div className="space-y-2 text-center">
-            <p>Photographer: {imageDetails.photographer}</p>
-            <p>Photographer <a href={imageDetails.photographer_url} target="_blank" rel="noreferrer">{imageDetails.photographer_url}</a></p>
-            <p>Image URL: <a href={imageDetails.url} target="_blank" rel="noreferrer">{imageDetails.url}</a></p>
-            <p>Dimensions: {imageDetails.width} x {imageDetails.height}</p>
-          </div>
-          <br></br>
-       
-        {isImageFullscreen && (
-          <div 
-            className="fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-75 flex justify-center items-center" 
-            onClick={toggleFullscreen} 
-          >
-            <div className="relative">
-              <img 
-                src={imageDetails.src.original} 
-                alt={imageDetails.alt} 
-                className="h-screen mx-auto" 
-              />
-              <button 
-                onClick={(e) => {
-                  e.stopPropagation(); 
-                  toggleFullscreen();
-                }} 
-                className="absolute top-4 right-4 text-white text-3xl focus:outline-none"
-              >
-                ×
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
+        </div>
+      )}
+    </div>
   );
 };
 
--- client/pages/dashboard/admin.js
+++ client/pages/dashboard/admin.js
@@ -4,6 +4,10 @@ import { supabase } from '../../supabase';
 export default function Admin() {
   const [users, setUsers] = useState([]);
   const [filter, setFilter] = useState('all');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [accountType, setAccountType] = useState('user');
 
   useEffect(() => {
     fetchUsers();
@@ -14,7 +18,7 @@ export default function Admin() {
       .from('users')
       .select('*');
     if (error) {
-      console.error('Erreur lors de la récupération des utilisateurs:', error);
+      console.error('Error fetching users:', error);
     } else {
       setUsers(data);
     }
@@ -27,33 +31,103 @@ export default function Admin() {
       .match({ id: userId });
 
     if (error) {
-      console.error('Erreur lors de la suppression de l\'utilisateur:', error);
+      console.error('Error deleting user:', error);
     } else {
       setUsers(users.filter(user => user.id !== userId));
-      console.log('Utilisateur supprimé avec succès:', data);
+      console.log('User deleted successfully:', data);
     }
   };
 
-  const filteredUsers = users.filter(user => filter === 'all' || user.type_compte === filter);
+  const filteredUsers = users.filter(user => filter === 'all' || user.account_type === filter);
+  
+  const handleSignUp = async (e) => {
+    e.preventDefault();
 
+    try {
+      const passwordHash = password; 
+
+      const { data, error } = await supabase
+        .from('users')
+        .insert([
+          { email: email, username: username, password_hash: passwordHash, type_compte: accountType },
+        ]);
+
+      if (error) {
+        throw error;
+      }
+
+      console.log('Inscription réussie:', data);
+      router.push('/dashboard/users');
+    } catch (error) {
+      console.error('Une erreur s\'est produite lors de l\'inscription:', error.message);
+    }
+  };
+  
   return (
     <div className={`bg-light dark:bg-dark`}>
       <h1 className="h1">Compte Admin</h1>
-      <div className="paragraphe">
-        Cette application est écrite dans le cadre du cours de Tech Web de l'ECE Paris par Greg, Olivier et Marc
+      <div className="p">
+       Ajout utilisateur
       </div>
+      <div className="mt-4 overflow-hidden  sm:rounded-lg">
+        <form className="" onSubmit={handleSignUp}>
+          <select onChange={(e) => setAccountType(e.target.value)} value={accountType }>
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+          </select>
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Nom d'utilisateur"
+            type="text"
+            className="ml-4"
+          />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            type="email"
+            className="ml-4"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Mot de passe"
+            type="password" 
+            className="ml-4"
+          />
+          <button onClick={() => { if (window.confirm('Êtes-vous sûr de vouloir ajouter cet utilisateur ?')) deleteUser(user.id) }}type="submit" className="text-blue-600 hover:text-blue-900 ml-4">
+            Ajouter utilisateur
+          </button>
+        </form>
+      </div>
+      <br></br>
+      <div className="p">
+       Gestion utilisateur 
+      </div>
+      <br></br>
       <select onChange={(e) => setFilter(e.target.value)}>
         <option value="all">Tous</option>
         <option value="admin">Admin</option>
         <option value="user">User</option>
       </select>
-      <div>
-        {filteredUsers.map(user => (
-          <div key={user.id}>
-            <p>Nom d'utilisateur : {user.username}</p> <button onClick={() => deleteUser(user.id)}>Supprimer</button>
-          </div>
-        ))}
+      <div className="mt-4 overflow-hidden shadow-md sm:rounded-lg">
+        <table className="min-w-full divide-y divide-gray-200">
+          {filteredUsers.map(user => (
+            <tr key={user.id} className="bg-white">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{user.type_compte}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{user.username}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{user.email}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+              <button onClick={() => { if (window.confirm('Êtes-vous sûr de vouloir supprimer cet utilisateur ?')) deleteUser(user.id) }} className="text-red-600 hover:text-red-900">
+                Supprimer
+              </button>
+              </td>
+            </tr>
+          ))}
+        </table>
       </div>
     </div>
   );
 }
+
