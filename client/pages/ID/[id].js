bc0c938b7146bcd1302f8f0c7977fad386478397
Olivier Texier <olivier@MacBook-Pro-de-Olivier.local>
2023-11-09 11:20:20 +0100

    Feat: banque d'image mise a jour automatiquement et ajout des vues

--- client/pages/ID/[id].js
+++ client/pages/ID/[id].js
@@ -6,49 +6,54 @@ import supabase from '../../supabase';
 
 const ImageDetail = () => {
   const router = useRouter();
-  const { id } = router.query; 
+  const { id } = router.query;
   const [imageDetails, setImageDetails] = useState(null);
   const [isImageFullscreen, setIsImageFullscreen] = useState(false);
 
-  const checkAndUpdateViews = async (apiImageId) => {
+  const addImageToDatabase = async (apiImageId) => {
     try {
-      const { data, error } = await supabase
+      const { data: selectData, error: selectError } = await supabase
         .from('images')
         .select('*')
-        .eq('api_image_id', apiImageId);
+        .eq('api_image_id', apiImageId)
+        .single();
   
-      if (error) {
-        console.error('Error fetching image:', error);
-        return;
-      }
-  
-      if (data.length > 0) {
-        const imageData = data[0];
-        const { error: updateError } = await supabase
-          .from('images')
-          .update({ views: imageData.views + 1 })
-          .eq('api_image_id', apiImageId);
-  
-        if (updateError) {
-          console.error('Error updating image views:', updateError);
-        }
-      } else {
+      if (!selectData) {
+        console.log('ID non trouvé. Ajout d\'une nouvelle entrée dans la table.');
         const { error: insertError } = await supabase
           .from('images')
           .insert([{ api_image_id: apiImageId, views: 1 }]);
   
         if (insertError) {
-          console.error('Error inserting new image:', insertError);
+          console.error('Erreur lors de l’insertion de la nouvelle image :', insertError);
+        } else {
+          console.log('Nouvelle entrée ajoutée avec succès !');
+        }
+      } else {
+        console.log('ID déjà existant dans la table.');
+        const { error: updateError } = await supabase
+          .from('images')
+          .update({ views: selectData.views + 1 })
+          .eq('api_image_id', apiImageId);
+  
+        if (updateError) {
+          console.error('Erreur lors de la mise à jour des vues de l’image :', updateError);
+        } else {
+          console.log('Vues mises à jour avec succès !');
         }
       }
     } catch (error) {
-      console.error('Error in checkAndUpdateViews:', error);
+      console.error('Erreur dans addImageToDatabase :', error);
     }
   };
+  
+  useEffect(() => {
+    if (id) {
+      addImageToDatabase(id);
+    }
+  }, [id]);
 
   useEffect(() => {
-    if (!router.isReady || !id) return;
-
     const fetchImageDetails = async () => {
       const apiKey = getAPIKey();
       const baseUrl = getAPIBaseURL();
@@ -56,16 +61,18 @@ const ImageDetail = () => {
 
       try {
         const response = await axios.get(url, {
-          headers: { Authorization: apiKey }
+          headers: { Authorization: apiKey },
         });
         setImageDetails(response.data);
-        checkAndUpdateViews(id);
+
       } catch (error) {
         console.error('Fetching image details failed:', error);
       }
     };
 
-    fetchImageDetails();
+    if (router.isReady && id) {
+      fetchImageDetails();
+    }
   }, [id, router.isReady]);
 
   useEffect(() => {
@@ -111,44 +118,56 @@ const ImageDetail = () => {
   return (
     <div className="body">
       <div className="relative">
-        <p className="text-center"><strong>Image Name:</strong> {imageDetails.alt}</p>
-        <button 
-          onClick={() => handleDownload(imageDetails.src.original, `${imageDetails.alt}.jpeg`)}
-          className="absolute top-0 right-0 bg-gray-800 text-white px-2 py-1 rounded-md text-xs"
+        <p className="text-center">
+          <strong>Image Name:</strong> {imageDetails.alt}
+        </p>
+        <button
+          onClick={() =>
+            handleDownload(imageDetails.src.original, `${imageDetails.alt}.jpeg`)
+          }
+          className="download-button absolute top-0 right-0 bg-gray-800 text-white px-2 py-1 rounded-md text-xs"
         >
           Download Image
         </button>
       </div>
       <div onClick={() => setIsImageFullscreen(!isImageFullscreen)} className="cursor-pointer">
-        <img 
-          src={imageDetails.src.original} 
-          alt={imageDetails.alt} 
-          className="w-auto h-auto object-cover mx-auto max-h-screen mb-4 cursor-pointer border-4 border-custom1" 
+        <img
+          src={imageDetails.src.original}
+          alt={imageDetails.alt}
+          className="image-preview w-auto h-auto object-cover mx-auto max-h-screen mb-4 cursor-pointer border-4 border-custom1"
         />
       </div>
       <div className="space-y-2 text-center">
         <p>Photographer: {imageDetails.photographer}</p>
-        <p>Photographer URL: <a href={imageDetails.photographer_url} target="_blank" rel="noreferrer">{imageDetails.photographer_url}</a></p>
-        <p>Image URL: <a href={imageDetails.url} target="_blank" rel="noreferrer">{imageDetails.url}</a></p>
-        <p>Dimensions: {imageDetails.width} x {imageDetails.height}</p>
+        <p>
+          Photographer URL:{' '}
+          <a href={imageDetails.photographer_url} target="_blank" rel="noreferrer">
+            {imageDetails.photographer_url}
+          </a>
+        </p>
+        <p>
+          Image URL:{' '}
+          <a href={imageDetails.url} target="_blank" rel="noreferrer">
+            {imageDetails.url}
+          </a>
+        </p>
+        <p>
+          Dimensions: {imageDetails.width} x {imageDetails.height}
+        </p>
       </div>
       {isImageFullscreen && (
-        <div 
-          className="fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-75 flex justify-center items-center" 
-          onClick={() => setIsImageFullscreen(false)} 
+        <div
+          className="fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-75 flex justify-center items-center"
+          onClick={() => setIsImageFullscreen(false)}
         >
           <div className="relative">
-            <img 
-              src={imageDetails.src.original} 
-              alt={imageDetails.alt} 
-              className="h-screen mx-auto" 
-            />
-            <button 
+            <img src={imageDetails.src.original} alt={imageDetails.alt} className="fullscreen-image h-screen mx-auto" />
+            <button
               onClick={(e) => {
-                e.stopPropagation(); 
+                e.stopPropagation();
                 setIsImageFullscreen(false);
-              }} 
-              className="absolute top-4 right-4 text-white text-3xl focus:outline-none"
+              }}
+              className="close-button absolute top-4 right-4 text-white text-3xl focus:outline-none"
             >
               ×
             </button>
