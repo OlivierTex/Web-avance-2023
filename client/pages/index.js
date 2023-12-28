da9d84c665a603b130498450923ccb009bb64591
Lotharim <greg.demirdjian@aim.com>
2023-12-28 13:48:33 +0100

    fix: correctif erreur DOM page d'accueil & isr, formatage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,32 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import { getAPIKey, getAPIBaseURL } from "../API/API_pexels";
 import { useRouter } from "next/router";
 import Marquee from "react-marquee-slider";
 import times from "lodash/times";
+import { StyleSheetManager } from "styled-components";
+import isPropValid from "@emotion/is-prop-valid";
+
+function ImageCarousel({ images, handleImageClick }) {
+  return (
+    <Marquee velocity={15}>
+      {times(images.length, Number).map((id) => (
+        <div
+          key={id}
+          className="flex justify-center"
+          style={{ margin: "10px" }}
+        >
+          <img
+            src={images[id].src.large}
+            alt={`Random ${images[id].id}`}
+            className="rounded shadow-lg cursor-pointer h-80 object-cover"
+            onClick={() => handleImageClick(images[id].id)}
+          />
+        </div>
+      ))}
+    </Marquee>
+  );
+}
 
 function Home({
   initialRandomImages1,
   initialRandomImages2,
   initialRandomImages3,
 }) {
-  const [randomImages1, setRandomImages1] = useState(initialRandomImages1);
-  const [randomImages2, setRandomImages2] = useState(initialRandomImages2);
-  const [randomImages3, setRandomImages3] = useState(initialRandomImages3);
   const router = useRouter();
-
-  useEffect(() => {
-    const fetchRandomImages = async () => {
-      const newRandomImages1 = await fetchImages();
-      setRandomImages1(newRandomImages1);
-      const newRandomImages2 = await fetchImages();
-      setRandomImages2(newRandomImages2);
-      const newRandomImages3 = await fetchImages();
-      setRandomImages3(newRandomImages3);
-    };
-
-    fetchRandomImages();
-  }, []);
 
   const handleImageClick = (id) => {
     router.push(`/image/${id}`);
@@ -41,66 +48,29 @@ function Home({
         <p className="text-xl text-gray-600 dark:text-gray-300 mt-2 mb-5">
           Discover our selection of high-quality photos and videos.
         </p>
-
-        <Marquee velocity={15}>
-          {times(randomImages1.length, Number).map((id) => (
-            <div
-              key={id}
-              className="flex justify-center"
-              style={{ margin: "10px" }}
-            >
-              <img
-                src={randomImages1[id].src.large}
-                alt={`Random ${randomImages1[id].id}`}
-                className="rounded shadow-lg cursor-pointer h-80 object-cover"
-                onClick={() => handleImageClick(randomImages1[id].id)}
-              />
-            </div>
-          ))}
-        </Marquee>
-
-        <Marquee velocity={15}>
-          {times(randomImages2.length, Number).map((id) => (
-            <div
-              key={id}
-              className="flex justify-center"
-              style={{ margin: "10px" }}
-            >
-              <img
-                src={randomImages2[id].src.large}
-                alt={`Random ${randomImages2[id].id}`}
-                className="rounded shadow-lg cursor-pointer h-80 object-cover"
-                onClick={() => handleImageClick(randomImages2[id].id)}
-              />
-            </div>
-          ))}
-        </Marquee>
-
-        <Marquee velocity={15}>
-          {times(randomImages3.length, Number).map((id) => (
-            <div
-              key={id}
-              className="flex justify-center"
-              style={{ margin: "10px" }}
-            >
-              <img
-                src={randomImages3[id].src.large}
-                alt={`Random ${randomImages3[id].id}`}
-                className="rounded shadow-lg cursor-pointer h-80 object-cover"
-                onClick={() => handleImageClick(randomImages3[id].id)}
-              />
-            </div>
-          ))}
-        </Marquee>
+        <StyleSheetManager shouldForwardProp={isPropValid}>
+          <ImageCarousel
+            images={initialRandomImages1}
+            handleImageClick={handleImageClick}
+          />
+          <ImageCarousel
+            images={initialRandomImages2}
+            handleImageClick={handleImageClick}
+          />
+          <ImageCarousel
+            images={initialRandomImages3}
+            handleImageClick={handleImageClick}
+          />
+        </StyleSheetManager>
       </div>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const initialRandomImages1 = await fetchImages();
-  const initialRandomImages2 = await fetchImages();
-  const initialRandomImages3 = await fetchImages();
+  const initialRandomImages1 = await fetchImagesWithFallback();
+  const initialRandomImages2 = await fetchImagesWithFallback();
+  const initialRandomImages3 = await fetchImagesWithFallback();
 
   return {
     props: {
@@ -108,11 +78,11 @@ export async function getStaticProps() {
       initialRandomImages2,
       initialRandomImages3,
     },
-    revalidate: 60,
+    revalidate: 60 * 5,
   };
 }
 
-async function fetchImages() {
+async function fetchImagesWithFallback() {
   const randomPage = Math.floor(Math.random() * 1000) + 1;
   const baseUrl = getAPIBaseURL();
   const apiUrl = `${baseUrl}curated?per_page=10&page=${randomPage}`;
@@ -124,9 +94,22 @@ async function fetchImages() {
         Authorization: apiKey,
       },
     });
-    return response.data.photos;
+
+    const photos = response.data.photos;
+
+    if (!photos || photos.length === 0) {
+      console.warn("Fetched data is empty. Trying again.");
+      return await fetchImagesWithFallback();
+    }
+
+    return photos;
   } catch (error) {
-    console.error(error);
+    if (error.response && error.response.status === 401) {
+      console.error("Unauthorized access. Please check your API key.");
+    } else {
+      console.error("Error fetching images:", error.message);
+    }
+
     return [];
   }
 }
